855f311f73878c315640e4a36a55e9304dc93bbd
Kauane Santos <kauanesantos.coding@gmail.com>
2023-12-03 18:16:12 -0300

    created header

---
+++ src/Components/Header/header.js
@@ -0,0 +1,21 @@
+import './style.css';
+
+
+function Header() {
+    return (
+        <div className="conatiner-header">
+            <div className='bar'>
+                <span>ks</span>
+
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div >
+    );
+}
+
+export default Header;
