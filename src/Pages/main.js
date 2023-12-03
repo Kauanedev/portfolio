55845f33dcd4e5a5a3ea88c7f171832894fe6237
Kauane Santos <kauanesantos.coding@gmail.com>
2023-12-03 18:16:03 -0300

    updating main

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -1,16 +1,28 @@
 import './style.css';
 import profile from '../Assets/profile.png'
+import Header from '../Components/Header/header';
 
 
 function Main() {
   return (
-    <div className="Main">
-      <img src={profile}
-        alt="profile"
-        className="profile"
-      />
+    <>
+      <Header />
 
-    </div>
+      <div className="conatiner-main">
+        <img src={profile}
+          alt="profile"
+          className="profile"
+        />
+
+        <div className='title'>
+          <span>👋Hello World!</span>
+          <h1>Kauane Santos</h1>
+          <h2>FullStack Developer</h2>
+        </div>
+
+      </div >
+
+    </>
   );
 }
 
