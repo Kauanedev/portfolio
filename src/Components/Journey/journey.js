5feee763ed88a83dd4a8e4297b32188e145aceb9
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-12 19:12:08 -0300

    created journey section

---
+++ src/Components/Journey/journey.js
@@ -0,0 +1,36 @@
+import './style.css';
+
+
+function Journey2() {
+    return (
+        <div className='container-journey'>
+
+            <div className='journey'>
+                <span id='journey'>🗓️ My Journey</span>
+
+                <div className='journey-boxes'>
+                    <div>
+                        <h2>Bachelor's Degree in Computer Engineering</h2>
+                        <p>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
+                        </p>
+                        <span>2023 - 2028</span>
+                    </div>
+                    <div>
+                        <h2>Fullstack Software Developer Certificate</h2>
+                        <p>
+                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
+                        </p>
+                        <span>04/2023 - 01/2024</span>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Journey2;
