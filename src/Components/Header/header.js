6648feb681a0b151bbe2d2aac88e94b02b1e7cad
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-12 18:48:13 -0300

    updated header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,16 +5,19 @@ function Header() {
     return (
         <div className="conatiner-header">
             <div className='bar'>
-                <a href="home">
+                <a href="home" className='logo'>
                     <span>ks</span>
                 </a>
 
-                <ul>
-                    <a href="#home">Home</a>
-                    <a href="#about">About me</a>
-                    <a>Home</a>
-                    <a>Home</a>
-                </ul>
+                <div className='references'>
+                    <ul>
+                        <a href="#home">Home</a>
+                        <a href="#about">About me</a>
+                        <a href='#journey'>Journey</a>
+                        <a>Home</a>
+                    </ul>
+                </div>
+
             </div>
         </div >
     );
