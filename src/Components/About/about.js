9686c220c50af169137261f7135c8bb639e76dfd
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-12 19:10:59 -0300

    created about section

---
+++ src/Components/About/about.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+
+function About() {
+    return (
+        <div className='container-about'>
+            <div className='about'>
+                <span id='about'>🧐 About Me</span>
+                <h5>👋My name is Kauane Santos but you can call me "Kau".</h5>
+                <h5>📍Born and raised in the capital of Rio de Janeiro, Brazil.</h5>
+                <h5>👩‍💻Studied software developement at <a href='https://cubos.academy/' target='_blank' rel="noreferrer">Cubos Academy</a>.</h5>
+                <h5>🎓Currently studying Computer Engineering at <a href='https://www.uninter.com/' target='_blank' rel="noreferrer">Uninter</a>.</h5>
+                <h5>🗓️In January 2023 had my first contact with programming and began my journey as a software developer student.</h5>
+            </div>
+        </div>
+
+    );
+}
+
+export default About;
