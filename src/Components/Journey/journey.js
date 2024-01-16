155cd5abb3e2785008cecb08fc064f129185761f
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-16 17:31:38 -0300

    updated journey

--- src/Components/Journey/journey.js
+++ src/Components/Journey/journey.js
@@ -1,33 +1,28 @@
+import journeyInfo from './journeyInfo';
 import './style.css';
 
 
 function Journey2() {
     return (
         <div className='container-journey'>
+            <span id='journey'>🎓 My Journey</span>
 
             <div className='journey'>
-                <span id='journey'>🗓️ My Journey</span>
-
-                <div className='journey-boxes'>
-                    <div>
-                        <h2>Bachelor's Degree in Computer Engineering</h2>
-                        <p>
-                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
-                        </p>
-                        <span>2023 - 2028</span>
-                    </div>
-                    <div>
-                        <h2>Fullstack Software Developer Certificate</h2>
-                        <p>
-                            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
-                        </p>
-                        <span>04/2023 - 01/2024</span>
-                    </div>
-
-                </div>
-
+                <>
+                    {journeyInfo.map((info) => {
+                        return (
+                            <div className='journey-box' key={info.id}>
+                                <h2>{info.title}</h2>
+                                <p>{info.description}</p>
+                                <span>{info.date}</span>
+                            </div>
+                        )
+                    })}
+                </>
 
             </div>
+
+
         </div>
 
     );
---
+++ src/Components/Journey/journeyInfo.js
@@ -0,0 +1,16 @@
+const journeyInfo = [
+    {
+        id: 1,
+        title: "Bachelor's Degree in Computer Engineering",
+        description: "Bachelor's Degree in Computer Engineering from the University of Waterloo.",
+        date: '2023 - 2028'
+    },
+    {
+        id: 2,
+        title: "Fullstack Software Developer Certificate",
+        description: "Fullstack Software Developer Certificate from the University of Waterloo.",
+        date: '03/2023 - 01/2024'
+    }
+]
+
+export default journeyInfo;
