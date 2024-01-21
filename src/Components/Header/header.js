d05e2e6eccf5015f5b4c6f0cafa4278a0dfd162b
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-21 19:47:50 -0300

    updated assets

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,10 +3,10 @@ import './style.css';
 
 function Header() {
     return (
-        <div className="conatiner-header">
+        <div className="container-header">
             <div className='bar'>
-                <a href="home" className='logo'>
-                    <span>ks</span>
+                <a id="home" className='logo' href='https://github.com/Kauanedev' target='_blank' rel="noreferrer">
+                    <h1>ks</h1>
                 </a>
 
                 <div className='references'>
@@ -14,7 +14,8 @@ function Header() {
                         <a href="#home">Home</a>
                         <a href="#about">About me</a>
                         <a href='#journey'>Journey</a>
-                        <a>Home</a>
+                        <a href='#projects'>Projects</a>
+                        <a href='#tecnologies'>Tecnologies</a>
                     </ul>
                 </div>
 
