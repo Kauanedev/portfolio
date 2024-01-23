304a21d0db76c1247503a898a91c961394de2f5d
Kauane Santos <kauanesantos.coding@gmail.com>
2024-01-23 18:49:19 -0300

    updated header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,11 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './style.css';
 
 
 function Header() {
     return (
         <div className="container-header">
-            <div className='bar'>
-                <a id="home" className='logo' href='https://github.com/Kauanedev' target='_blank' rel="noreferrer">
+            <navbar>
+                <a className='logo' href='http://localhost:3000/'>
                     <h1>ks</h1>
                 </a>
 
@@ -13,13 +14,14 @@ function Header() {
                     <ul>
                         <a href="#home">Home</a>
                         <a href="#about">About me</a>
-                        <a href='#journey'>Journey</a>
                         <a href='#projects'>Projects</a>
+                        <a href='#journey'>Journey</a>
                         <a href='#tecnologies'>Tecnologies</a>
+                        <a href='#contacts'>Contacts</a>
                     </ul>
                 </div>
 
-            </div>
+            </navbar>
         </div >
     );
 }
